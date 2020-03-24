97b782e22a65a513a3d53b12a3e7f167c4da1c3c
vohongson48 <vohongson48@gmail.com>
2020-03-24 22:36:53 +0700

    Xin phep

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,13 @@
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class DemoApplication {
 
     private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
@@ -13,6 +15,7 @@ public class DemoApplication {
     public static void main(String[] args) {
         logger.info("this is a info message");
         SpringApplication.run(DemoApplication.class, args);
+
     }
 
 }
--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -2,22 +2,25 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Student;
 import com.example.demo.service.StudentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@Slf4j
 public class StudentController {
     private final StudentService studentService;
 
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
+
     }
 
     @RequestMapping("/student")
     public List<Student> findAll(){
-
         return studentService.findAll();
+        log.info
     }
 }
